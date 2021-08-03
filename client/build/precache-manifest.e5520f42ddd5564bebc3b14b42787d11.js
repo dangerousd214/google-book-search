2d99cb13c18144d259ce6b0b1d4f0c197ed3d561
Darren Watson <v_tec@live.com>
2021-08-03 12:46:13 -0500

    ran build

---
+++ client/build/precache-manifest.e5520f42ddd5564bebc3b14b42787d11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c16ba50bdbf402d1731b374e7d4335",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c26ddb5bb8db832c76d2",
+    "url": "/static/css/main.174b53f3.chunk.css"
+  },
+  {
+    "revision": "fabf0341df1681839e85",
+    "url": "/static/js/2.58baefc2.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.58baefc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26ddb5bb8db832c76d2",
+    "url": "/static/js/main.dee62a22.chunk.js"
+  },
+  {
+    "revision": "816cf679577cc3650d0c",
+    "url": "/static/js/runtime-main.87c4f50d.js"
+  }
+]);
