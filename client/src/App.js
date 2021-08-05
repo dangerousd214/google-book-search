cf231cdecc778299770abcdb7d2dc47e5549a107
Darren Watson <v_tec@live.com>
2021-08-05 17:49:50 -0500

    ok one more time

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
-import Saved from "./pages/Saved";
-import NoMatch from "./pages/NoMatch";
+import Saved from "./pages/saved";
+import NoMatch from "./pages/nomatch";
 import Nav from "./components/Nav";
 
 function App() {
