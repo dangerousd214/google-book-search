6986bd5b26fb17ea24ce9807142953a9b4c36582
Darren Watson <v_tec@live.com>
2021-09-02 18:29:26 -0500

    added a google verification for api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export default {
   // Gets books from the Google API
   getBooks: function(q) {
-    return axios.get("/api/google", { params: { q: "title:" + q } });
+    console.log("Search:",  q)
+    return axios.get("/api/google", { params: { q: q } });
   },
   // Gets all saved books
   getSavedBooks: function() {
